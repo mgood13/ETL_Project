acfb538a205c3388076a226c4082998bdfdf7c20
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2020-12-12 13:46:23 -0500

    SQL Schema for the database

---
+++ cars_tables.sql
@@ -0,0 +1,46 @@
+Create table cars (
+id int,
+Manufacturer Varchar(30),
+Model Varchar(30),
+Transmission Varchar(30),
+Color Varchar(30),
+Odometer int,
+Year int, 
+Engine_Type Varchar(30),
+Engine_Capacity float,
+Body Varchar(30),
+State Varchar(30),
+Drivetrain Varchar(30),
+Price float,
+List_Duration int
+);
+
+Create Table sara_cars(
+id int,
+price int, 
+mark varchar(30),
+model varchar(30), 
+year int, 
+mileage int,
+engine_capacity int, 
+transmission varchar(30), 
+drive varchar(30), 
+hand_drive varchar(30), 
+fuel varchar(30)
+);
+
+Create table vehicles (
+Manufacturer varchar(30),
+Model varchar(30),
+Vehicle_Type varchar(30),
+Engine_Size float,
+Horsepower float,
+Wheelbase float,
+Width float,
+Length float,
+Curb_Weight float,
+Fuel_Capacity float,
+Fuel_Efficiency float
+)
+
+select * from  vehicles
